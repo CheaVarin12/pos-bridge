284ad185a4eb7c8e10eb27307dc49f077dd89103
CheaVarin12 <cheavarin7@gmail.com>
2026-01-20 21:18:40 +0700

    fix

--- run-bridge.bat
+++ run-bridge.bat
@@ -1,4 +1,16 @@
 @echo off
-cd /d %~dp0
-start "" "%~dp0php\php-win.exe" "%~dp0bridge.php"
-exit
+TITLE POS Print Bridge
+
+:: Navigate to the folder where this file is
+cd /d "%~dp0"
+
+echo ---------------------------------------
+echo  POS PRINTER BRIDGE
+echo ---------------------------------------
+echo.
+
+:: This uses the PHP already installed on your laptop
+php bridge.php
+
+:: If it crashes, keep the window open so you can see why
+pause
